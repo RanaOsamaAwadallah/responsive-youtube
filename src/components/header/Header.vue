95bd186f9bd99dcca804a4c39b9ccea6bc6f4cfc
rosama@raisaenergy.com <rosama@raisaenergy.com>
2020-06-20 20:45:49 +0200

    [NEW] Header and search component intial look

---
+++ src/components/header/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+    <img alt="Vue logo" src="../../assets/logo.png" />
+    <SearchBar />
+  </div>
+</template>
+
+<script>
+import SearchBar from "../shared/search/search.component";
+export default {
+  name: "Header",
+  components: {
+    SearchBar,
+  },
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  height: 56px;
+  display: flex;
+  justify-content: space-between;
+}
+img {
+  width: 80px;
+  height: 24px;
+}
+</style>
---
+++ src/components/shared/search/search.component.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="search-bar">
+    <input
+      class="search-bar-input"
+      type="text"
+      placeholder="Search"
+      aria-label="Search"
+    />
+    <button class="search-bar-button">
+      <font-awesome-icon icon="search" size="md" />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+$border-color: #cacaca;
+$border-radius: 0.188em;
+.search-bar {
+  display: flex;
+  height: 32px;
+  margin-left: 5%;
+  width: 100%;
+  .search-bar-input {
+    flex-grow: 9;
+    border: 1px solid $border-color;
+    border-right: none;
+    border-top-left-radius: $border-radius;
+    border-bottom-left-radius: $border-radius;
+    padding: 0 1em;
+  }
+  .search-bar-button {
+    flex-grow: 1;
+    border: 1px solid $border-color;
+    border-top-right-radius: $border-radius;
+    border-bottom-right-radius: $border-radius;
+    background-color: #ededed;
+    &:active,
+    &:focus {
+      background-color: #d3d3d3;
+      border: 1px solid $border-color;
+    }
+  }
+}
+</style>
