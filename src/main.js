a89816a6a9745b93ff3e37fd340bec5240b01a94
rosama@raisaenergy.com <rosama@raisaenergy.com>
2020-06-20 19:51:09 +0200

    [CONFIG] Adding font awesome for icons

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {
+  library
+} from "@fortawesome/fontawesome-svg-core";
+import {
+  faSearch
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  FontAwesomeIcon,
+  FontAwesomeLayers,
+  FontAwesomeLayersText,
+} from "@fortawesome/vue-fontawesome";
 
-Vue.config.productionTip = false
+library.add(faSearch);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+Vue.component("font-awesome-layers", FontAwesomeLayers);
+Vue.component("font-awesome-layers-text", FontAwesomeLayersText);
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
