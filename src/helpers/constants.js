9cfbb89e3f6db46651195b8c81202d2ffa160422
rosama@raisaenergy.com <rosama@raisaenergy.com>
2020-06-23 10:55:31 +0200

    [FIX] Fixing constatns

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,6 +1,5 @@
 export const VUE_GOOGLE_API_CONFIG = {
-    apiKey: "AIzaSyB0UxYZ8HjkoYaOngP5oxX3KkvN2DG6w5M", //en
-    // apiKey: "AIzaSyAq4NO5Zf6lGSokZlK-1cbcUUr6jhtRFiI", ro
+    apiKey: "API_KEY"
 };
 export const YOUTUBE_CHANNEL_TYPE = 'youtube#channel';
 export const YOUTUBE_VIDEO_TYPE = 'youtube#video';
