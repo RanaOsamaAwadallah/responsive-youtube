14c1c07d7f0741d5c45096252aaf25eb1b9fe6a2
rosama@raisaenergy.com <rosama@raisaenergy.com>
2020-07-02 14:05:26 +0200

    [NEW] Related videos in video details page

--- src/views/video-page.vue
+++ src/views/video-page.vue
@@ -1,40 +1,99 @@
 <template>
   <div class="video-page" :class="{ mobile: isMobile() }">
     <div class="video-page__video">
-      <LazyYoutubeVideo :src="`https://www.youtube.com/embed/${this.$route.params.id}`" />
+      <LazyYoutubeVideo :src="`https://www.youtube.com/embed/${$route.params.id}`" />
     </div>
-    <div class="video-page__detalis"></div>
-    <div class="video-page__related-videos"></div>
+    <div class="video-page__detalis">
+      <div class="video-title">{{videoTitle}}</div>
+      <div class="channel-title">{{channelTitle}}</div>
+    </div>
+    <div class="video-page__related-videos">
+      <SearchResult :isMobile="isMobile()" :videos="relatedVideos" />
+    </div>
   </div>
 </template>
 
 <script>
 import { mixinDetictingMobile } from "../helpers/mixins";
 import LazyYoutubeVideo from "vue-lazy-youtube-video";
+import SearchResult from "../components/search-result-list/search-result-list";
+import { mapSearchListToChannelAndVideoList } from "../helpers/mappers";
 
 export default {
   name: "VideoPage",
-  components: { LazyYoutubeVideo },
+  components: { LazyYoutubeVideo, SearchResult },
   mixins: [mixinDetictingMobile],
+  props: ["videos"],
   data: function() {
     return {
-      video: Object
+      relatedVideos: Array,
+      isLoading: Boolean,
+      errorMessage: String
     };
   },
-  created() {
-    this.$gapi
-      .request({
-        path: "https://www.googleapis.com/youtube/v3/videos",
-        method: "GET",
-        params: {
-          id: this.$route.params.id
-        }
-      })
-      .then(response => {
-        this.video = response;
-      });
+  computed: {
+    video() {
+      return this.videos.find(video => video.id === this.$route.params.id);
+    },
+    videoTitle() {
+      return this.videos ? this.video.title : "";
+    },
+    channelTitle() {
+      return this.videos ? this.video.channelTitle : "";
+    }
+  },
+  mounted() {
+    this.getRelatedVideos();
+  },
+  methods: {
+    getRelatedVideos() {
+      const _this = this;
+      this.$gapi
+        .request({
+          path: `https://www.googleapis.com/youtube/v3/search`,
+          method: "GET",
+          params: {
+            part: "snippet",
+            type: "video",
+            id: this.$route.params.id
+          }
+        })
+        .then(response => {
+          const responseLists = mapSearchListToChannelAndVideoList(response);
+
+          if (typeof _this.relatedVideos !== "object") {
+            _this.relatedVideos = responseLists.videosList;
+          } else {
+            _this.relatedVideos.push(...responseLists.videosList);
+          }
+          _this.nextPageToken = responseLists.nextPageToken || "";
+          _this.isLoading = false;
+          _this.errorMessage = "";
+        })
+        .catch(response => {
+          _this.errorMessage = response.result.error.message;
+        });
+    }
   }
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import "../assets/styles/_variables.scss";
+
+.video-page__detalis {
+  border-bottom: 1px solid #dedede;
+  line-height: 2rem;
+  padding: 10px 0;
+
+  .video-title {
+    font-weight: normal;
+    font-size: 1.5rem;
+  }
+
+  .channel-title {
+    color: $faded-font-color;
+    font-size: 1.2rem;
+  }
+}
+</style>
