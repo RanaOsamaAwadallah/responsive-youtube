0451f58165d82320503eecc2d93674baf7eb8bb9
rosama@raisaenergy.com <rosama@raisaenergy.com>
2020-07-04 19:37:11 +0200

    [FIX] handle scroll not working on some screens fix

--- src/helpers/services.js
+++ src/helpers/services.js
@@ -1,7 +1,9 @@
 export const handleScrollToBottom = (element, callBackFn, callBackValue) => {
-    let bottomOfWindow = element.scrollTop === element.scrollHeight - element.offsetHeight;
-
-    if (bottomOfWindow) {
-        callBackFn(callBackValue);
-    }
-}
+  let bottomOfWindow =
+    Math.abs(
+      element.scrollTop - (element.scrollHeight - element.offsetHeight)
+    ) < 5;
+  if (bottomOfWindow) {
+    callBackFn(callBackValue);
+  }
+};
